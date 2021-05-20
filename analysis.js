f8730c23cb1e255ca0da961bef6f396c6bf9566c
volodymyrsyt <82583082+volodymyrsyt@users.noreply.github.com>
2021-05-20 12:38:03 +0300

    Add files via upload

---
+++ analysis.js
@@ -0,0 +1,30 @@
+// JScript source code
+{
+    //Ввод строки
+    var str = document.inp.m.value;
+    //Преобразование строки в число
+    var int = parseInt(str);
+    //Проверка
+    if (int == 1 || int == 2 || int == 12)
+    { alert("Зима"); }
+    else if (3 <= int && int <= 5)
+    { alert("Весна"); }
+    else if (6 <= int && int <= 8)
+    { alert("Лето"); }
+    else if (9 <= int && int <= 11)
+    { alert("Осень"); }
+    else
+    { alert("Неверно!"); }
+    if (int == 1 || int == 2 || int == 12)
+    { alert("Зима"); }
+    else if (3 <= int && int <= 5)
+    { alert("Весна"); }
+    else if (6 <= int && int <= 8)
+    { alert("Лето"); }
+    else if (9 <= int && int <= 11)
+    { alert("Осень"); }
+    else
+    { alert("Неверно!"); }
+
+    
+    }
